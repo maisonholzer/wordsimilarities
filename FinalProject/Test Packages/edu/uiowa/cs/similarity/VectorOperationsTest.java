6e2a86645f65a85e9835ade446aa5b4d2664068a
yinhsu <yinhsu@172.17.26.168>
2017-12-08 23:52:49 -0600

    Final_plus_extra_credit_working_testsadded

--- FinalProject/Test Packages/edu/uiowa/cs/similarity/VectorOperationsTest.java
+++ FinalProject/Test Packages/edu/uiowa/cs/similarity/VectorOperationsTest.java
@@ -20,15 +20,12 @@ public class VectorOperationsTest {
      */
     @Test
     public void testNegEuc() {
-        System.out.println("negEuc");
+        
         // build map1
         TreeMap<String, Integer> map1 = new TreeMap<>();
         map1.put("a", 1);
         map1.put("b", 4);
         map1.put("c", 1);
-        //map1.put("d", 0);
-        //map1.put("e", 0);
-        //map1.put("f", 0);
         
         // build map2
         TreeMap<String, Integer> map2 = new TreeMap<>();
@@ -50,21 +47,17 @@ public class VectorOperationsTest {
      */
     @Test
     public void testEucNorm() {
-        System.out.println("eucNorm");
+        
         // build map1
         TreeMap<String, Integer> map1 = new TreeMap<>();
         map1.put("a", 1);
         map1.put("b", 4);
         map1.put("c", 1);
-        //map1.put("d", 0);
-        //map1.put("e", 0);
-        //map1.put("f", 0);
         
         // build map2
         TreeMap<String, Integer> map2 = new TreeMap<>();
         map2.put("a", 3);
-        //map2.put("b", 0);
-        //map2.put("c", 0);
+        
         map2.put("d", 1);
         map2.put("e", 1);
         map2.put("f", 2);
@@ -73,6 +66,29 @@ public class VectorOperationsTest {
         double result = m.eucNorm(map1, map2);
         
         assertEquals(-1.27861316602, result, 0.00001);
+    }
+    
+    
+    // Test empty trees
+    @Test
+    public void testNegEucEmpty() {
+        TreeMap<String, Integer> map1 = new TreeMap<>();
+        TreeMap<String, Integer> map2 = new TreeMap<>();
+        
+        VectorOperations m = new VectorOperations();
+        double result = m.negEuc(map1, map2);
+        assertEquals(0, result, 0.00001);
+    }
+
+// Test empty trees
+    @Test
+    public void testEucNormEmpty() {
+        TreeMap<String, Integer> map1 = new TreeMap<>();
+        TreeMap<String, Integer> map2 = new TreeMap<>();
+        
+        VectorOperations m = new VectorOperations();
+        double result = m.eucNorm(map1, map2);
+        assertEquals(0, result, 0.00001);
     }
     
 }
